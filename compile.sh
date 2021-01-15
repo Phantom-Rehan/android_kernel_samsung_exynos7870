9bb7e8722037e070ee43c7ec4b497225c10f9685
Rehan Tadpatri <phantomrehan@outlook.com>
2021-01-15 22:29:08 +0530

    enabled ccache(2)

--- compile.sh
+++ compile.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # Custom compile script
 
-export CC=ccache clang
-export ARCH=arm64
-export SUBARCH=arm64
+# export CC=ccache clang
+# export ARCH=arm64
+# export SUBARCH=arm64
 export PATH=/home/phantom/dev/toolchain/bin:$PATH
 
-make ARCH=arm64 lineage-gtaxlwifi_defconfig
+# make ARCH=arm64 lineage-gtaxlwifi_defconfig
 make -j$(nproc --all) \
                       ARCH=arm64 \
-                      CC=clang \
+                      CC="ccache clang" \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=aarch64-linux-android- \
                       | tee kernel.log
